617d6a2ac1b5bf653aeb2cc8e13b6cdd7b33e009
Marg <paerl.mag@gmail.com>
2021-04-05 12:05:03 +0300

    refactor: rule 3.4

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task ";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
